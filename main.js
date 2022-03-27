dbc14c039e99bc5e400dae732845a7153bf7b1b9
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-03-28 01:46:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,8 @@
     $('<h1>').appendTo(head).text('MIDI分割');
     $('<h2>').appendTo(head).text('指定したタイミングでMIDIを分割');
     const rpgen3 = await importAll([
+        'https://rpgen3.github.io/maze/mjs/heap/Heap.mjs',
+        'https://rpgen3.github.io/piano/mjs/toMIDI.mjs',
         [
             'input',
             'css',
@@ -59,7 +61,7 @@
             g_midi = v;
         });
     }
-    {
+    const [isSplitDrum, isRemoveChord] = (() => {
         const {html} = addHideArea('setting flag');
         const isSplitDrum = rpgen3.addInputBool(html, {
             label: 'ドラムチャンネルを楽器ごとに分割',
@@ -71,8 +73,9 @@
             save: true,
             value: true
         });
-    }
-    {
+        return [isSplitDrum, isRemoveChord];
+    })();
+    const splitPoints = (() => {
         const {html} = addHideArea('setting split point');
         const inputSplitPoint = rpgen3.addInputStr(html, {
             label: '何小節目で分割するか'
@@ -89,9 +92,20 @@
                 list.delete(n);
             }).addClass('deleteBtn');
         }).addClass('btn');
-    }
+        return list;
+    })();
     rpgen3.addBtn(html, 'start split', () => {
-        parseMidi(g_midi);
+        const {timeDivision} = g_midi; // 4分音符の長さ
+        const [times, map] = splitLength(
+            timeDivision,
+            splitPoints,
+            splitChannel(
+                isSplitDrum(),
+                isRemoveChord(),
+                parseMidi(g_midi)
+            )
+        );
+        const bpm = getBPM(g_midi);
     }).addClass('btn');
     const getBPM = midi => {
         const {track} = midi;
@@ -107,13 +121,13 @@
         if(bpm) return bpm;
         else throw 'BPM is none.';
     };
-    const parseMidi = async midi => { // pitch, volume, duration
+    const parseMidi = midi => {
         const {track, timeDivision} = midi,
-              heap = new rpgen4.Heap();
+              heap = new rpgen3.Heap();
         for(const {event} of track) {
             const now = new Map;
             let currentTime = 0;
-            for(const {deltaTime, type, data, channel} of event) { // 全noteを回収
+            for(const {deltaTime, type, data, channel} of event) {
                 currentTime += deltaTime;
                 if(type !== 8 && type !== 9) continue;
                 const [pitch, velocity] = data,
@@ -121,7 +135,7 @@
                 if(now.has(pitch) && isNoteOFF) {
                     const unit = now.get(pitch);
                     unit.end = currentTime;
-                    heap.push(unit.start, unit);
+                    heap.add(unit.start, unit);
                     now.delete(pitch);
                 }
                 else if(!isNoteOFF) now.set(pitch, new MidiUnit({
@@ -132,21 +146,7 @@
                 }));
             }
         }
-        while(timeline.length) timeline.pop();
-        endTime = 0;
-        const deltaToSec = 60 / getBPM(midi) / timeDivision;
-        for(const {ch, pitch, velocity, start, end} of heap) {
-            const [_start, _end] = [start, end].map(v => v * deltaToSec);
-            timeline.push(new AudioUnit({
-                ch,
-                pitch,
-                volume: velocity / 0x7F,
-                when: _start,
-                duration: _end - _start
-            }));
-            if(endTime < _end) endTime = _end;
-        }
-        endTime += coolTime;
+        return heap;
     };
     class MidiUnit {
         constructor({ch, pitch, velocity, start}){
@@ -157,13 +157,55 @@
             this.end = -1;
         }
     }
-    class AudioUnit {
-        constructor({ch, pitch, volume, when, duration}){
-            this.ch = ch;
-            this.pitch = pitch;
-            this.volume = volume;
-            this.when = when;
-            this.duration = duration;
+    const makeSafelyGet = m => k => {
+        if(!m.has(k)) m.set(k, []);
+        return m.get(k);
+    };
+    const splitChannel = (isSplitDrum, isRemoveChord, heap) => {
+        const m = new Map,
+              get = makeSafelyGet(m);
+        for(const v of heap) {
+            const {ch, pitch} = v;
+            if(ch === 9 && isSplitDrum) get(`${ch}-${pitch}`).push(v);
+            else {
+                const a = get(ch);
+                if(ch !== 9 && isRemoveChord && a.length) {
+                    const _v = a[a.length - 1];
+                    if(v.start === _v.start) {
+                        if(v.end < _v.end) a.pop();
+                        else continue;
+                    }
+                }
+                a.push(v);
+            }
         }
-    }
+        return m;
+    };
+    const splitLength = (timeDivision, splitPoints, channels) => {
+        const bar = timeDivision * 4,
+              end = Math.max(...[...channels.values()].filter(v => v.length).map(v => v[v.length - 1].end)) / bar,
+              times = [0, ...[...splitPoints].filter(v => 0 < v && v < end).sort((a, b) => a - b).concat(end).map(v => v * bar)],
+              m = new Map;
+        for(const [k, v] of channels) {
+            const a = new Map,
+                  get = makeSafelyGet(a);
+            m.set(k, a);
+            let i = 0;
+            for(const _v of v) {
+                const {start, end} = _v;
+                while(start >= times[i]) i++;
+                get(times[i - 1]).push(_v);
+                let w = _v,
+                    j = i;
+                while(end > times[j]) {
+                    const _w = {...w};
+                    w.end = _w.start = times[j];
+                    get(times[j]).push(_w);
+                    w = _w;
+                    j++;
+                }
+            }
+        }
+        return [times, m];
+    };
 })();
