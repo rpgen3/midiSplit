3e14074abc16cf16b8efd2a25dbe3514771e396e
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2022-03-28 21:50:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -71,7 +71,7 @@
             g_midi = v;
         });
     }
-    const [isSplitDrum, isRemoveChord, isShift] = (() => {
+    const [isSplitDrum, isRemoveChord, isShift, isReset] = (() => {
         const {html} = addHideArea('setting flag');
         const isSplitDrum = rpgen3.addInputBool(html, {
             label: 'ドラムを楽器ごとに分割',
@@ -88,7 +88,12 @@
             save: true,
             value: true
         });
-        return [isSplitDrum, isRemoveChord, isShift];
+        const isReset = rpgen3.addInputBool(html, {
+            label: 'トラックを連番で再割り当て',
+            save: true,
+            value: true
+        });
+        return [isSplitDrum, isRemoveChord, isShift, isReset];
     })();
     const splitPoints = (() => {
         const {html} = addHideArea('setting split point');
@@ -110,10 +115,12 @@
         return list;
     })();
     rpgen3.addBtn(main, 'start split', () => {
+        if(!g_midi) return table.text('Error: Must input MIDI file.');
         const {timeDivision} = g_midi; // 4分音符の長さ
         output(
             timeDivision,
             isShift(),
+            isReset(),
             getBPM(g_midi),
             splitLength(
                 timeDivision,
@@ -204,7 +211,7 @@
     const splitLength = (timeDivision, splitPoints, channels) => {
         const bar = timeDivision * 4,
               end = Math.max(...[...channels.values()].filter(v => v.length).map(v => v[v.length - 1].end)) / bar,
-              times = [0, ...[...splitPoints].filter(v => 0 < v && v < end).sort((a, b) => a - b).concat(end).map(v => v * bar)],
+              times = [0, ...[...splitPoints].map(v => v - 1).filter(v => 0 < v && v < end).sort((a, b) => a - b).concat(end).map(v => v * bar)],
               map = new Map;
         for(const [k, v] of channels) {
             const m = new Map,
@@ -229,31 +236,38 @@
         times.pop();
         return [times, map];
     };
-    const output = (timeDivision, isShift, bpm, [times, map]) => {
+    const calcColor = (rgb, max, value) => rgb + (value / max * 0xFF | 0).toString(16);
+    const toMidiChannel = str => {
+        const ch = String(str);
+        return Number(ch.includes('-') ? ch.split('-')[0] : ch);
+    }
+    const output = (timeDivision, isShift, isReset, bpm, [times, map]) => {
         table.empty();
         const bar = timeDivision * 4,
-              max = Math.max(...[...map.values()].map(v => [...v.values()].map(v => v.length)).flat()),
+              max = 2 * Math.max(...[...map.values()].map(v => [...v.values()].map(v => v.length)).flat()),
               tr = $('<tr>').appendTo($('<thead>').appendTo(table));
         $('<th>').appendTo(tr);
-        for(const v of times) $('<th>').appendTo(tr).text(v / bar);
-        const tbody = $('<tbody>').appendTo(table);
-        for(const ch of [...map.keys()].sort()) {
-            const m = map.get(ch);
-            const tr = $('<tr>').appendTo(tbody);
+        for(const v of times) $('<th>').appendTo(tr).text(v / bar + 1);
+        const tbody = $('<tbody>').appendTo(table),
+              sorted = [...map.keys()].sort();
+        for(const ch of sorted) {
+            const m = map.get(ch),
+                  tr = $('<tr>').appendTo(tbody);
             $('<th>').appendTo(tr).text(ch);
             for(const time of times) {
-                const td = $('<td>').appendTo(tr);
                 if(!m.has(time)) continue;
                 const t = time / bar,
-                      a = m.get(time);
-                td.text(a.length).css({
-                    backgroundColor: '#73B8E2' + (a.length / max * 0xFF | 0).toString(16)
+                      a = toMidiTrack(m.get(time), time);
+                if(isShift) {
+                    const {when} = a[0];
+                    for(const v of a) v.when -= when;
+                }
+                $('<td>').appendTo(tr).text(a.length).css({
+                    backgroundColor: calcColor('#73B8E2', max, a.length)
                 }).on('click', () => {
-                    const _ch = String(ch),
-                          __ch = Number(_ch.includes('-') ? _ch.split('-')[0] : _ch);
                     rpgen3.download(
                         rpgen4.toMIDI({
-                            tracks: [[__ch, toMidiTrack(a, time, isShift)]],
+                            tracks: [[toMidiChannel(ch), a]],
                             bpm,
                             div: timeDivision
                         }),
@@ -262,8 +276,36 @@
                 });
             }
         }
+        {
+            const max = 2 * Math.max(...times.map(time => [...map.values()].filter(v => v.has(time)).reduce((p, x) => p + x.get(time).length, 0))),
+                  tr = $('<tr>').appendTo(tbody);
+            console.log(max)
+            $('<th>').appendTo(tr).text('all');
+            for(const time of times) {
+                const tracks = [];
+                let i = 0;
+                for(const ch of sorted) {
+                    const m = map.get(ch);
+                    if(!m.has(time)) continue;
+                    tracks.push([isReset ? i++ : toMidiChannel(ch), toMidiTrack(m.get(time), time)]);
+                }
+                const len = tracks.reduce((p, x) => p + x[1].length, 0);
+                $('<td>').appendTo(tr).text(len).css({
+                    backgroundColor: calcColor('#f3981d', max, len)
+                }).on('click', () => {
+                    rpgen3.download(
+                        rpgen4.toMIDI({
+                            tracks,
+                            bpm,
+                            div: timeDivision
+                        }),
+                        `midiSplit - all at ${time}.mid`
+                    );
+                });
+            }
+        }
     };
-    const toMidiTrack = (units, time, isShift) => {
+    const toMidiTrack = (units, time) => {
         const heap = new rpgen4.Heap();
         for(const {
             pitch,
@@ -280,11 +322,6 @@
                 when: v
             });
         }
-        const a = [...heap];
-        if(isShift) {
-            const {when} = a[0];
-            for(const v of a) v.when -= when;
-        }
-        return rpgen4.fixTrack(a);
+        return rpgen4.fixTrack([...heap]);
     };
 })();
